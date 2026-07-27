243e131ce12567f897057d630bd66f92cc864872
Leticia Spak <leticiamiyamoto550@gmail.com>
2026-07-27 16:46:53 -0300

    Add files via upload

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>Explore das tabs</Text>
+    </View>
+  );
+}
+
+const s = StyleSheet.create({});
---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TabThreeScreen() {
+  return (
+    <View>
+      <Text>List das tabs</Text>
+    </View>
+  );
+}
+
+const s = StyleSheet.create({});
